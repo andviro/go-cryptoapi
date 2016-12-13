210b341ba6b702e9faf8b27b53c872ff3d18b20d
Andrew Rodionoff <andviro@gmail.com>
2016-12-13 21:45:48 +0300

    restored some libraries

--- csp/common.go
+++ csp/common.go
@@ -4,8 +4,8 @@ package csp
 
 /*
 #cgo linux CFLAGS: -I/opt/cprocsp/include/cpcsp
-#cgo linux,amd64 LDFLAGS: -L/opt/cprocsp/lib/amd64/ -lcapi10 -lcapi20
-#cgo linux,386 LDFLAGS: -L/opt/cprocsp/lib/ia32/ -lcapi10 -lcapi20
+#cgo linux,amd64 LDFLAGS: -L/opt/cprocsp/lib/amd64/ -lcapi10 -lcapi20 -lrdrsup -lssp
+#cgo linux,386 LDFLAGS: -L/opt/cprocsp/lib/ia32/ -lcapi10 -lcapi20 -lrdrsup -lssp
 #cgo windows LDFLAGS: -lcrypt32 -lpthread
 #include "common.h"
 */
