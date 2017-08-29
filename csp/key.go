d583eb0c3162cdfdb91f059eb8a3e2750ec59f7b
leboev <konstantin.leboev@gmail.com>
2017-08-29 16:58:16 +0300

    Added Key method GetParam
    It is possible now to retrieve certificate from container.

--- csp/key.go
+++ csp/key.go
@@ -3,6 +3,10 @@ package csp
 //#include "common.h"
 import "C"
 
+import (
+	"unsafe"
+)
+
 // KeyFlag sets options on created key pair
 type KeyFlag C.DWORD
 
@@ -18,6 +22,13 @@ type KeyPairId C.DWORD
 const (
 	AtKeyExchange KeyPairId = C.AT_KEYEXCHANGE
 	AtSignature   KeyPairId = C.AT_SIGNATURE
+)
+
+// KeyParam represents key parameters that can be retrieved for key.
+type KeyParamId C.DWORD
+
+const (
+	KeyCertificateParam KeyParamId = C.KP_CERTIFICATE // X.509 certificate that has been encoded by using DER
 )
 
 // Key incapsulates key pair functions
@@ -46,6 +57,24 @@ func (ctx Ctx) GenKey(at KeyPairId, flags KeyFlag) (res Key, err error) {
 		err = getErr("Error creating key for container")
 		return
 	}
+	return
+}
+
+// GetParam retrieves data that governs the operations of a key.
+func (key Key) GetParam(param KeyParamId) (res []byte, err error) {
+	var slen C.DWORD
+	if C.CryptGetKeyParam(key.hKey, C.DWORD(param), nil, &slen, 0) == 0 {
+		err = getErr("Error getting param's value length for key")
+		return
+	}
+
+	buf := make([]byte, slen)
+	if C.CryptGetKeyParam(key.hKey, C.DWORD(param), (*C.BYTE)(unsafe.Pointer(&buf[0])), &slen, 0) == 0 {
+		err = getErr("Error getting param for key")
+		return
+	}
+
+	res = buf[0:int(slen)]
 	return
 }
 
